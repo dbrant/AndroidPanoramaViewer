a87ea1f810b13e52d7a2da0e83bdaf5df70bb9b1
Dmitry Brant <dbrant@wikimedia.org>
2020-10-12 17:54:38 -0400

    Correctly call super.

--- src/main/java/com/dmitrybrant/photo360/MainActivity.java
+++ src/main/java/com/dmitrybrant/photo360/MainActivity.java
@@ -121,6 +121,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
         setIntent(intent);
         recreate();
     }
